a220a72838e907cce710ce5cecc452b2d7fafc42
RamVignesh B <ramvigneshgreat@pec.edu>
2020-06-03 22:14:40 +0530

    Create Two_City_Scheduling.js

---
+++ June/Two_City_Scheduling.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+var twoCitySchedCost = function(costs) {
+    costs.sort((a,b) => {
+        return Math.abs(b[0] - b[1]) - Math.abs(a[0] - a[1]);
+    });
+    let sum = 0
+    let cityA = 0
+    let cityB = 0;
+    
+    for (let i = 0; i < costs.length; i++) {
+        let costA = costs[i][0];
+        let costB = costs[i][1];
+        
+        //if A costs less
+        if (costA <= costB) {
+            if (cityA < costs.length / 2) {
+               sum += costA;
+                cityA++; 
+            }
+            else {
+                sum += costB;
+                cityB++;
+            }
+        }
+        // B costs less
+        else {
+            if (cityB < costs.length / 2) {
+                sum += costB;
+                cityB++;
+            }
+            else {
+                sum += costA;
+                cityA++;
+            }
+        }
+    }
+    return sum;
+};
